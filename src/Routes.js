554ba4bcd3ff55ca325076aed6b5fa02640f3f10
insungsong <song22861@naver.com>
2020-10-22 15:43:33 +0900

    .

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom"
 import App from "./App"
 import SignIn from "./SignIn"
 import SignUp from "./SignUp"
-import SignUpFail from "./SignupFail";
+import SignProcessFail from "./SignProcessFail";
 
 export default () => {
     return (
@@ -11,7 +11,7 @@ export default () => {
             <Route exact path="/" component={App} />
             <Route path="/signIn" component={SignIn} />
             <Route path="/signupsuccess" component={SignUp} />
-            <Route path="/signupfail" component={SignUpFail} />
+            <Route path="/signupfail" component={SignProcessFail} />
         </Switch>
     )
 }
