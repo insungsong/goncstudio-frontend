a372acc576e266dee9f59804adeea6a5ca024525
insungsong <song22861@naver.com>
2020-10-22 15:24:49 +0900

    page error

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom"
 import App from "./App"
 import SignIn from "./SignIn"
 import SignUp from "./SignUp"
+import SignUpFail from "./SignupFail"
 
 export default () => {
     return (
@@ -10,7 +11,7 @@ export default () => {
             <Route exact path="/" component={App} />
             <Route path="/signIn" component={SignIn} />
             <Route path="/signupsuccess" component={SignUp} />
-            <Route path="/signupfail" component={SignUp} />
+            <Route path="/signupfail" component={SignUpFail} />
         </Switch>
     )
 }
